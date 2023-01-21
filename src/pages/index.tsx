5afe716f069dd61a8348ba6488679188e29b27dc
Renato Mignogna <renmignogna@gmail.com>
2023-01-21 22:35:40 +0000

    add a header component boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
     <>
+    <Header />
       <div>
         <h1 className="flex text-2xl justify-center font-bold text-purple-800">Next Movies</h1>
       </div>
