1d93e25c9b2cf6aca0308b5ade384c2741a68730
Renato Mignogna <renmignogna@gmail.com>
2023-01-25 02:38:09 +0000

    add next auth, add login logout buttons in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,35 @@
 import React, { useState, FC, useEffect } from "react"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Header: FC = () => {
+  const { data: session } = useSession()
+  console.log("session >>>", session)
   return (
-    <header className="sticky text-white bg-[#041791] top-0 z-[1000] flex justify-end items-center px-10 md:px-12 h-[72px]">
+    <header className="sticky text-white bg-[#041791] top-0 z-[1000] flex justify-center sm:justify-end items-center px-4 md:px-6 h-[72px]">
       <div className="cursor-pointer">
-        <button type="submit" className="px-12 py-2 bg-[#334ae3] rounded-lg">
-          Login
-        </button>
+        {session && (
+          <div className="flex">
+            <div className="space-x-2">
+              <button className=" px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg">
+                Create Movie
+              </button>
+              <button
+                onClick={signOut}
+                className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg"
+              >
+                Sign out
+              </button>
+            </div>
+          </div>
+        )}
+        {!session && (
+          <button
+            onClick={signIn}
+            className="px-12 py-2 bg-[#334ae3] rounded-lg"
+          >
+            Sign in
+          </button>
+        )}
       </div>
     </header>
   )
