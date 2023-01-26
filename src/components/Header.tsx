7f036e3a1677e0ee73b114e774969a4a8634a1fc
Renato Mignogna <renmignogna@gmail.com>
2023-01-26 13:11:10 +0000

    conditional header button for better user experience, styling to create movies page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,26 +5,51 @@ import { useRouter } from "next/router"
 const Header: FC = () => {
   const { data: session } = useSession()
   const router = useRouter()
+  console.log(router.asPath)
+  const isOnCreateMoviePage = router.asPath === "/create-movie"
+
   return (
     <header className="sticky text-white bg-[#041791] top-0 z-[1000] flex justify-center sm:justify-end items-center px-4 md:px-6 h-[72px]">
       <div>
         {session && (
           <div className="flex">
             <div className="space-x-2">
-              <button
-                onClick={() => {
-                  router.push("/create-movie")
-                }}
-                className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
-              >
-                Create Movie
-              </button>
-              <button
-                onClick={() => signOut()}
-                className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
-              >
-                Sign out
-              </button>
+              {!isOnCreateMoviePage && (
+                <>
+                  <button
+                    onClick={() => {
+                      router.push("/create-movie")
+                    }}
+                    className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
+                  >
+                    <p>Create Movie</p>
+                  </button>
+                  <button
+                    onClick={() => signOut()}
+                    className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
+                  >
+                    <p>Sign out</p>
+                  </button>
+                </>
+              )}
+              {isOnCreateMoviePage && (
+                <>
+                  <button
+                    onClick={() => {
+                      router.push("/")
+                    }}
+                    className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
+                  >
+                    <p>Home</p>
+                  </button>
+                  <button
+                    onClick={() => signOut()}
+                    className="px-4 md:px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
+                  >
+                    <p>Sign out</p>
+                  </button>
+                </>
+              )}
             </div>
           </div>
         )}
--- src/pages/create-movie.tsx
+++ src/pages/create-movie.tsx
@@ -3,6 +3,7 @@ import { useMutation, useQuery, useQueryClient } from "react-query"
 import { useRouter } from "next/router"
 import { useSession, signIn } from "next-auth/react"
 import { createMovie } from "@/mutations/createMovie"
+import Header from "@/components/Header"
 import { Formik, Form, Field } from "formik"
 import { TextField } from "@/components/TextField"
 import { TextArea } from "@/components/TextArea"
@@ -51,90 +52,99 @@ const CreateMovie = () => {
     router.push("./")
   }
   return (
-    <div className="text-center pt-20 pb-20 h-full bg-[#323235]">
-      <h1 className="text-white text-4xl text-bold">Create A Movie</h1>
-      <div className="">
-        {!session && (
-          <div>
+    <>
+      <div>
+        <Header />
+      </div>
+      <div className="text-center pt-8 pb-20 h-full bg-[#041791]">
+        <h1 className="text-white text-4xl text-bold">Create A Movie</h1>
+        <div className="">
+          {!session && (
             <div>
-              <h2 className="text-white mt-8 text-lg">
-                Please Sign In to create a movie
-              </h2>
+              <div>
+                <h2 className="text-white mt-8 text-lg">
+                  Please Sign In to create a movie
+                </h2>
+              </div>
+              <div className="mt-8">
+                <button
+                  onClick={() => signIn()}
+                  className="px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
+                >
+                  Sign in
+                </button>
+              </div>
             </div>
-            <div className="mt-8">
-              <button
-                onClick={() => signIn()}
-                className="px-12 py-2 bg-[#334ae3] rounded-lg cursor-pointer"
-              >
-                Sign in
-              </button>
+          )}
+        </div>
+        {session && (
+          <Formik
+            initialValues={initialValues}
+            onSubmit={async (values) => {
+              handleSubmit(values)
+            }}
+          >
+            <div className="flex justify-center mt-6 w-5/6 md:max-w-[650px] m-auto text-start rounded-lg md:px-5 py-20 bg-[#f0f0f0]">
+              <Form>
+                <div className="flex flex-col md:flex-row md:space-x-5">
+                  <div>
+                    <TextField label="Title" name="title" type="text" />
+                    <TextArea
+                      label="Description"
+                      name="description"
+                      type="text"
+                    />
+                    <TextField label="Starring" name="starring" type="text" />
+                    <TextField label="Duration" name="duration" type="text" />
+                    <TextField
+                      label="Age Rating"
+                      name="ageRating"
+                      type="text"
+                    />
+                  </div>
+                  <div>
+                    <TextField label="Year" name="year" type="text" />
+                    <TextField label="Trailer" name="trailer" type="text" />
+                    <label htmlFor="genres" className="">
+                      Choose Genres
+                    </label>
+                    <Field
+                      component="select"
+                      id="genres"
+                      name="genres"
+                      multiple={true}
+                      className="h-48 mb-2 form-control mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                   focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                   disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                   invalid:border-pink-500 invalid:text-pink-600
+                   focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    >
+                      <option value="Action">Action</option>
+                      <option value="Adventure">Adventure</option>
+                      <option value="Thriller">Thriller</option>
+                      <option value="Comedy">Comedy</option>
+                      <option value="Science Fiction">Science Fiction</option>
+                      <option value="War">War</option>
+                      <option value="Crime">Crime</option>
+                      <option value="Drama">Drama</option>
+                      <option value="Family">Family</option>
+                      <option value="Fantasy">Fantasy</option>
+                    </Field>
+                    <TextField label="Image URL" name="imageUrl" type="text" />
+                  </div>
+                </div>
+                <button
+                  className="w-full border border-black mt-5 px-12 py-3"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </Form>
             </div>
-          </div>
+          </Formik>
         )}
       </div>
-      {session && (
-        <Formik
-          initialValues={initialValues}
-          onSubmit={async (values) => {
-            handleSubmit(values)
-          }}
-        >
-          <div className="flex justify-center mt-6 w-5/6 md:max-w-[650px] m-auto text-start rounded-lg px-5 py-6 bg-[#f0f0f0]">
-            <Form>
-              <div className="flex flex-col md:flex-row md:space-x-5">
-                <div>
-                  <TextField label="Title" name="title" type="text" />
-                  <TextArea
-                    label="Description"
-                    name="description"
-                    type="text"
-                  />
-                  <TextField label="Starring" name="starring" type="text" />
-                  <TextField label="Duration" name="duration" type="text" />
-                  <TextField label="Age Rating" name="ageRating" type="text" />
-                </div>
-                <div>
-                  <TextField label="Year" name="year" type="text" />
-                  <TextField label="Trailer" name="trailer" type="text" />
-                  <label htmlFor="genres" className="">
-                    Choose Genres
-                  </label>
-                  <Field
-                    component="select"
-                    id="genres"
-                    name="genres"
-                    multiple={true}
-                    className="h-48 mb-2 form-control mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                     focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                     invalid:border-pink-500 invalid:text-pink-600
-                     focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-                  >
-                    <option value="Action">Action</option>
-                    <option value="Adventure">Adventure</option>
-                    <option value="Thriller">Thriller</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Science Fiction">Science Fiction</option>
-                    <option value="War">War</option>
-                    <option value="Crime">Crime</option>
-                    <option value="Drama">Drama</option>
-                    <option value="Family">Family</option>
-                    <option value="Fantasy">Fantasy</option>
-                  </Field>
-                  <TextField label="Image URL" name="imageUrl" type="text" />
-                </div>
-              </div>
-              <button
-                className="w-full border border-black mt-5 px-12 py-3"
-                type="submit"
-              >
-                Submit
-              </button>
-            </Form>
-          </div>
-        </Formik>
-      )}
-    </div>
+    </>
   )
 }
 
