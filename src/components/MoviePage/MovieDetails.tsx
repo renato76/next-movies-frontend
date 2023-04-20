02e5e016dea0c24181f6a60d8026339ac98c66a6
Renato Mignogna <renmignogna@gmail.com>
2023-04-20 22:47:05 +0100

    add user score

--- src/components/MoviePage/MovieDetails.tsx
+++ src/components/MoviePage/MovieDetails.tsx
@@ -8,6 +8,7 @@ import Modal from "../Modal"
 import MovieForm from "../MovieForm"
 import { FaPlay, FaEdit } from "react-icons/fa"
 import { AiOutlineHome } from "react-icons/ai"
+import { BsDot } from "react-icons/bs"
 import Reviews from "./Reviews"
 
 const MovieDetails = ({ movie }: MovieApiResponse) => {
@@ -19,6 +20,21 @@ const MovieDetails = ({ movie }: MovieApiResponse) => {
 
   const handleEditMovie = () => {
     setShowMovieForm(true)
+  }
+
+  const getAverageRating = () => {
+    if (
+      movie.data.attributes.reviews &&
+      movie.data.attributes.reviews.data.length > 0
+    ) {
+      const allRatingsArray = movie?.data?.attributes?.reviews?.data.map(
+        (review) => review?.attributes?.rating
+      )
+      const totalRatings = allRatingsArray?.reduce((acc, curr) => acc + curr)
+      return (totalRatings!! * 20) / allRatingsArray!.length
+    } else {
+      return 67
+    }
   }
 
   return (
@@ -66,9 +82,6 @@ const MovieDetails = ({ movie }: MovieApiResponse) => {
                   <h1>{movie.data.attributes.title}&nbsp;</h1>
                 </div>
               </div>
-              <div className="flex justify-center md:justify-start text-white">
-                <h1>({movie.data.attributes.year})</h1>
-              </div>
               <div className="text-white flex text-center py-5 justify-center md:justify-start">
                 {movie.data.attributes.genres.map((genre) => (
                   <div
@@ -80,10 +93,19 @@ const MovieDetails = ({ movie }: MovieApiResponse) => {
                 ))}
               </div>
               <div className="flex text-white items-center justify-center md:justify-start">
-                <div className="mr-5 text-center border-solid border-2 border-[#ffffffb7] px-2 py-1">
+                <div className="mr-2 text-center border-solid border-2 border-[#ffffffb7] px-2 py-1">
                   <p>{movie.data.attributes.ageRating}</p>
                 </div>
+                <span className="text-3xl">
+                  <BsDot />
+                </span>
                 <div>{movie.data.attributes.duration}</div>
+                <span className="text-3xl">
+                  <BsDot />
+                </span>
+                <div>
+                  <h1>{movie.data.attributes.year}</h1>
+                </div>
               </div>
               <div className="text-white flex pt-5 justify-between items-center mb-2">
                 <h3 className="font-bold text-xl pl-8 md:pl-0">Overview</h3>
@@ -102,16 +124,38 @@ const MovieDetails = ({ movie }: MovieApiResponse) => {
                   {movie.data.attributes.description}
                 </p>
               </div>
-              <div className="flex flex-col text-white items-start pl-8 md:pl-0 pb-8">
-                <a
-                  className="cursor-pointer flex items-center"
-                  href={movie.data.attributes.trailer}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaPlay />
-                  <p className="flex items-center hover:text-[#01b4e4] transition duration-500 ml-4">Play Trailer</p>
-                </a>
+              <div className="flex pb-8 items-center">
+                <div className="pl-8 md:pl-0">
+                  <div className="flex justify-center items-center mr-12">
+                    <div className="flex flex-col font-bold text-sm mr-2 text-white">
+                      <h4>
+                        User
+                      </h4>
+                      <h4>
+                        Score
+                      </h4>
+                    </div>
+                    <div className="bg-[#343434] h-[60px] w-[60px] mr-1 flex justify-center items-center border-4 border-[#878787] rounded-full">
+                      <h5 className="font-bold text-2xl text-[#dbdbdb]">
+                        {getAverageRating()}
+                      </h5>
+                      <span className="mb-2 text-[#d4d4d4] text-xs">%</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col text-white items-start pl-8 md:pl-0">
+                  <a
+                    className="cursor-pointer flex items-center"
+                    href={movie.data.attributes.trailer}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaPlay />
+                    <p className="flex items-center hover:text-[#01b4e4] transition duration-500 ml-4">
+                      Play Trailer
+                    </p>
+                  </a>
+                </div>
               </div>
               <div className="text-white flex flex-col pl-8 md:pl-0">
                 <div>
